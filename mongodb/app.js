e32d03a2d9939ea49a8a5c34dbc47993a1a160d2
Joshua Appiah Dadzie <48050933+joedemartinez@users.noreply.github.com>
2023-06-01 18:54:13 +0000

    update

--- mongodb/app.js
+++ mongodb/app.js
@@ -3,12 +3,14 @@ const { connectToDb, getDb } = require('./db')
 const bodyParser = require('body-parser');
 const cors = require('cors') //CORS policy: No 'Access-Control-Allow-Origin' h 
 const bcrypt = require('bcryptjs');
+const {ObjectId} = require('mongodb')
+
 
 //init app
 const app = express()
 app.use(bodyParser.json()) 
 app.use(cors()) //CORS policy: No 'Access-Control-Allow-Origin' 
-
+app.use(express.json())
 
 //db conn
 let db
@@ -188,3 +190,64 @@ app.get('/count/exits', (req, res) => {
         res.status(200).json({error: "Could not fetch"})
     })
 })
+
+//trial
+// app.get('/trial', (req, res) => {
+//     db.collection('units')
+//     .find({Name: 'Internal Audit'})
+//     .then(() => {
+//         res.status(200).json({status: true, data: exits})
+//     }).catch(()=> {
+//         res.status(200).json({error: "Could not fetch"})
+//     })
+// })
+// if (ObjectId.isValid(req.params.id))
+// app.get('/trial/:id', (req, res) => {
+//   if (ObjectId.isValid(req.params.id)){
+//     db.collection('units')
+//     .findOne({Name: 'Internal Audit'})
+//     .then((doc) => {
+//         res.status(200).json({status: true, data: doc})
+//     }).catch(()=> {
+//         res.status(200).json({error: "Could not fetch"})
+//     }}
+// })
+
+// //post
+// app.post('/trial', (req, res) => {
+//     const exits = req.body
+//     db.collection('units')
+//     .insertOne(exits)
+//     .then((result) => {
+//         res.status(200).json({status: true, data: result})
+//     }).catch(()=> {
+//         res.status(200).json({error: "Could not fetch"})
+//     })
+// })
+
+// //delete
+// app.delete('/trial:id', (req, res) => {
+//   if (ObjectId.isValid(req.params.id)){
+//     db.collection('units')
+//     .deleteOne({Name: 'Internal Audit'})
+//     .then((doc) => {
+//         res.status(200).json({status: true, data: doc})
+//     }).catch(()=> {
+//         res.status(200).json({error: "Could not fetch"})
+//     })
+// }
+// })
+
+// //update
+// app.patch('/trial:id', (req, res) => {
+//     const lupdate = req.body
+//     if (ObjectId.isValid(req.params.id)){
+//       db.collection('units')
+//       .updateOne({Name: 'Internal Audit'}, {$set: lupdate})
+//       .then((doc) => {
+//           res.status(200).json({status: true, data: doc})
+//       }).catch(()=> {
+//           res.status(200).json({error: "Could not fetch"})
+//       })
+//   }
+//   })
